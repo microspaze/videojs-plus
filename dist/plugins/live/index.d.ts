d2b16d07628a94d0446fcf947e279e4743ae32bd
Pong <samfunghp@gmail.com>
2020-04-07 08:43:06 +0800

    Updates

--- dist/plugins/live/index.d.ts
+++
@@ -1,9 +0,0 @@
-import videojs from 'video.js';
-
-declare module 'video.js' {
-  export interface LivePlugin extends videojs.Plugin {}
-
-  interface VideoJsPlayer {
-    live: () => LivePlugin;
-  }
-}
